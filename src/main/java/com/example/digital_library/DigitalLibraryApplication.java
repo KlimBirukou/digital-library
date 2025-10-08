9787421e3415073d99379e6f65774ed787b3027a
klimb <manulc0rparation@gmail.com>
2025-10-08 17:43:03 +0300

    Added changelog file for organization, extend changelog file for user. Fix mapping for liquibase

--- src/main/java/com/example/digital_library/DigitalLibraryApplication.java
+++ src/main/java/com/example/digital_library/DigitalLibraryApplication.java
@@ -1,9 +1,11 @@
 package com.example.digital_library;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.digital_library.mapper")
 public class DigitalLibraryApplication {
 
 	public static void main(String[] args) {
