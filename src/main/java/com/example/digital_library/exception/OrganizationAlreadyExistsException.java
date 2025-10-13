f5ecd9f3e93dfa0dd8a3ec423b1921ead51280f7
klimb <manulc0rparation@gmail.com>
2025-10-13 18:20:22 +0300

    Correct several code style mistakes

--- src/main/java/com/example/digital_library/exception/OrganizationAlreadyExistsException.java
+++ src/main/java/com/example/digital_library/exception/OrganizationAlreadyExistsException.java
@@ -1,6 +1,7 @@
 package com.example.digital_library.exception;
 
 public class OrganizationAlreadyExistsException extends RuntimeException {
+
     public OrganizationAlreadyExistsException(String name) {
         super("Organization with %s already exist".formatted(name));
     }
